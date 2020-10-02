76196ea5b2d8561c774921e9ce0172fa80331883
lelinh <lelinhmtatn@gmail.com>
2020-10-02 16:38:56 +0700

    XDmohinh3lop

---
+++ DAL/ketnoi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class ketnoi
+    {
+        public static SqlConnection Get()
+        {
+            SqlConnection cnn = new SqlConnection();
+            cnn.ConnectionString = @"Data Source=DESKTOP-5OFAVU9\SQLEXPRESS;Initial Catalog=quanlykhachsan;Integrated Security=True";
+            cnn.Open();
+            return cnn;
+        }
+   }
+}
